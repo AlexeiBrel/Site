38611e1a93e76caf4c2d218e1a95283b0acfd1a1
Aliaksei <brel.2000@mail.ru>
2022-01-15 15:26:55 +0300

    creation of the block and about us in numbers

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ let show = true;
 $(window).on("scroll load resize", function() {
     if (!show) return false;
     let w_top = $(window).scrollTop(),
-        e_top = $('.about-main').offset().top,
+        e_top = $('.benefits__header').offset().top,
         w_height = $(window).height(),
         d_height = $(document).height(),
-        e_height = $('.about-main').outerHeight();
+        e_height = $('.benefits__header').outerHeight();
     if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
         $('.benefits__number').css('opacity', '1');
         $('.benefits__number').spincrement({ thousandSeparator: "", duration: 8000 });
