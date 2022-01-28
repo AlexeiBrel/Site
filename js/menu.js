484289d89c8a6cca4b97751a84da1a6176975595
Aliaksei <brel.2000@mail.ru>
2022-01-28 17:55:53 +0300

    delete files and edit page index.html

--- js/menu.js
+++ js/menu.js
@@ -22,7 +22,7 @@ window.onload = function() {
     window.setTimeout(function() {
         document.body.classList.add('loaded');
         document.body.classList.remove('loaded_hiding');
-    }, 1500);
+    }, 1000);
 }
 
 // -----------initialization---------------//
